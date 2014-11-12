ac5b3198a0b41502a5792d6a7637b54a66634a42
Mu57Di3 <mu57di3@gmail.com>
2014-11-12 23:46:50 +0300

    Добавленна возможность стиминга видео, поворот видео и отслеживание размеров и смещения точки вражения при ресайзе видео плеера

--- js/app/player.js
+++ js/app/player.js
@@ -21,23 +21,64 @@ define(['screenfull.min','app/flash'],function () {
         this.display.init(id,function (){
             that._initGUI();
         });
+
     }
 
     Application.prototype = {
         display:null,
         curentQuality: 240,
         file:null,
+        status:0,
+
+        STOP:0,
+        PLAYED:1,
+        PAUSED:2,
+
+        rotateAngle:0,
+
+        elH: 0,
+        elW: 0,
+        dCP: 0,
 
         _initGUI: function () {
             var that = this;
 
+            this.elH = String($('#player_wrapper').css('height')).replace('px','');
+            this.elW = String($('#player_wrapper').css('width')).replace('px','');
+
+            this.dCP = Math.abs((this.elW/2)-(this.elH/2));
+
+
+            console.log('INIT GUI');
             $('#bPlay').one('click', function () {
                 console.log(that.file.files['mp4_'+that.curentQuality]);
                 that.play(that.file.files['mp4_'+that.curentQuality]);
+                that.status = that.PLAYED;
+                $(this).find('i.glyphicon ').addClass('glyphicon-pause');
                 $(this).on('click', function () {
                     that.toglePlay();
+                    
                 });
             });
+
+            //апдейтим размер плеера при изменении размера
+            screenfull.onchange = function(){
+                that.elH = String($('#player_wrapper').css('height')).replace('px','');
+                that.elW = String($('#player_wrapper').css('width')).replace('px','');
+
+                that.dCP = Math.abs((that.elW/2)-(that.elH/2));
+                if (that.rotateAngle == 90 || that.rotateAngle == 270){
+                    $(that.display.o).css('width',$('#player_wrapper').css('height'));
+                    $(that.display.o).css('height',$('#player_wrapper').css('width'));
+                    $(that.display.o).css('top','-'+that.dCP+'px');
+                    $(that.display.o).css('left',that.dCP+'px');
+                } else {
+                    $(that.display.o).css('width',$('#player_wrapper').css('width'));
+                    $(that.display.o).css('height',$('#player_wrapper').css('height'));
+                    $(that.display.o).css('top','0');
+                    $(that.display.o).css('left','0');
+                }
+            }
 
 
             $('#bFullScreen').on('click', function () {
@@ -50,6 +91,40 @@ define(['screenfull.min','app/flash'],function () {
                 that.progress(a,b);
 
             });
+
+            $(this.display).on('adapter.stop',function(e){
+                console.log(1111);
+                that.status = that.STOP;
+                $('#bPlay').off('click');
+                $('#bPlay').find('i.glyphicon ').removeClass('glyphicon-pause');
+                $('#bPlay').find('i.glyphicon ').removeClass('glyphicon-play');
+                $('#bPlay').find('i.glyphicon ').addClass('glyphicon-repeat');
+
+                $('#bPlay').one('click',function (){
+                    $(this).find('i.glyphicon ').removeClass('glyphicon-repeat');
+                    $(this).find('i.glyphicon ').addClass('glyphicon-pause');
+                    $(this).on('click', function () {
+                        that.toglePlay();
+                    });
+                    that.replay();
+                });
+            });
+
+            $('.progress').on('click',function (e){
+
+                if (that.status > 0){
+                    var cx = e.clientX,
+                        ep = 40,
+                        ew = String($('.progress').css('width')).replace('px',''),
+                        dw = Math.round((cx-ep)/ew*10000)/10000,
+                        time = that.file.duration*dw;
+                    that.seek(time);
+                }
+            })
+
+            $('#bRotate').on('click',function(){
+                that.rotate();
+            });
         },
 
         play: function(file) {
@@ -58,6 +133,15 @@ define(['screenfull.min','app/flash'],function () {
 
         toglePlay:function (){
             this.display.o.pp();
+            $('#bPlay').find('i.glyphicon ').removeClass('glyphicon-pause');
+            $('#bPlay').find('i.glyphicon ').removeClass('glyphicon-play');
+            if (this.status == this.PLAYED){
+                this.status = this.PAUSED;
+                $('#bPlay').find('i.glyphicon ').addClass('glyphicon-play');
+            } else if (this.status == this.PAUSED){
+                this.status = this.PLAYED
+                $('#bPlay').find('i.glyphicon ').addClass('glyphicon-pause');
+            }
         },
 
         volume: function(val){
@@ -68,6 +152,11 @@ define(['screenfull.min','app/flash'],function () {
             this.display.o.seek(val);
         },
 
+        replay:function(){
+            this.display.o.replay();
+            this.status = this.PLAYED;
+        },
+
         chahgeQuality:function (){
 
         },
@@ -75,6 +164,41 @@ define(['screenfull.min','app/flash'],function () {
         progress:function(t,s){
             var td = Math.round(t/this.file.duration*10000)/100;
             $('.progress-bar').css('width',td+'%');
+        },
+
+        rotate:function(){
+            var elH = 0, elW= 0, dCP = 0;
+
+
+            this.elH = String($('#player_wrapper').css('height')).replace('px','');
+            this.elW = String($('#player_wrapper').css('width')).replace('px','');
+
+            this.dCP = Math.abs((this.elW/2)-(this.elH/2));
+
+            console.log(dCP);
+
+            this.rotateAngle+=90;
+            this.rotateAngle= this.rotateAngle ==360 ? 0 : this.rotateAngle;
+
+            if (this.rotateAngle == 90 || this.rotateAngle == 270){
+                $(this.display.o).css('width',$('#player_wrapper').css('height'));
+                $(this.display.o).css('height',$('#player_wrapper').css('width'));
+                $(this.display.o).css('top','-'+this.dCP+'px');
+                $(this.display.o).css('left',this.dCP+'px');
+            } else {
+                $(this.display.o).css('width',$('#player_wrapper').css('width'));
+                $(this.display.o).css('height',$('#player_wrapper').css('height'));
+                $(this.display.o).css('top','0');
+                $(this.display.o).css('left','0');
+            }
+
+            $(this.display.o).css('mozTransform','rotate('+this.rotateAngle+'deg)');
+            $(this.display.o).css('transform','rotate('+this.rotateAngle+'deg)');
+            $(this.display.o).css('webkitTransform','rotate('+this.rotateAngle+'deg)');
+            $(this.display.o).css('msTransform','rotate('+this.rotateAngle+'deg)');
+            $(this.display.o).css('oTransform','rotate('+this.rotateAngle+'deg)');
+
+
         }
 
     }
